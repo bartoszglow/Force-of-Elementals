a9f2ed7e1a1ffbf0ff6bf18f41a224dc4c8c6c61
joinee <bartosz.glow@gmail.com>
2014-08-21 20:00:19 +0200

    Configuration of grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,21 +4,46 @@ module.exports = function(grunt) {
 	    pkg: grunt.file.readJSON('package.json'),
 	    uglify: {
 		    options: {
-		        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+		        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
 		    },
 		    js: {
-		        src: [
-		        		'app/js/*.js',
-		        	],
-		        dest: 'build/js/build.js',
-		    },
+		        src: 'app/js/*.js',
+		        dest: 'build/js/build.js'
+		    }
 	    },
+		watch: {
+		  	js: {
+				files: [
+						'app/js/**/*.js',
+		 		],
+				options: { livereload: true }
+	  		},
+		  	html: {
+		  		files: [
+		  				'app/**/*.html', 
+		  		],
+				options: { livereload: true }
+		  	}
+		},
+		copy: {
+			main: {
+			    expand: true,
+			    cwd: 'app/',
+			    src: [
+			    		'index.html'
+			    ],
+			    dest: 'build/',
+			    flatten: false
+			}
+		}
 	});
 
 	  // Load plugins
 	  grunt.loadNpmTasks('grunt-contrib-uglify');
+	  grunt.loadNpmTasks('grunt-contrib-watch');
+	  grunt.loadNpmTasks('grunt-contrib-copy');
 
 
 	  // Default task(s).
-	  grunt.registerTask('default', ['uglify']);
+	  grunt.registerTask('default', ['copy', 'uglify', 'watch']);
 };
