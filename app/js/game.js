6b7f7bc06a42872bf768a2e25997b91a8dcfc4b9
joinee <bartosz.glow@gmail.com>
2014-08-21 19:37:16 +0200

    Added folders structure

---
+++ app/js/game.js
@@ -0,0 +1,10 @@
+var cos = function(test) {
+	var 
+		test2 = test;
+
+	return test2;
+
+
+
+	// TEST COMMENT
+}
---
+++ build/js/build.js
@@ -0,0 +1,2 @@
+/*! Force-of-Elementals 2014-08-21 */
+var cos=function(a){var b=a;return b};
