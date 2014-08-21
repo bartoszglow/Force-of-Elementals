f323f673374ca6d57dd2145e605f14b50e833557
joinee <bartosz.glow@gmail.com>
2014-08-21 21:36:23 +0200

    Updated grunt with livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,27 +11,38 @@ module.exports = function(grunt) {
 		        dest: 'build/js/build.min.js'
 		    }
 	    },
+		express: {
+		    all: {
+		        options: {
+		            bases: ['build'],
+		            port: 8080,
+		            hostname: "0.0.0.0",
+		            livereload: true
+		        }
+		    }
+		},
 		watch: {
-		  	js: {
-				files: [
-						'app/js/**/*.js',
-		 		],
-				options: { livereload: true }
-	  		},
-		  	html: {
-		  		files: [
-		  				'app/**/*.html', 
-		  		],
-				options: { livereload: true }
-		  	}
+			app: {
+	        	files: 'app/**/*',
+	        	tasks: ['copy', 'uglify'],
+		    },
+		    build: {
+		        files: 'build/*',
+	            options: {
+	                livereload: true
+		        }
+		    }
+		},
+		open: {
+		    all: {
+		        path: 'http://localhost:8080/index.html'
+		    }
 		},
 		copy: {
 			main: {
 			    expand: true,
 			    cwd: 'app/',
-			    src: [
-			    		'index.html'
-			    ],
+			    src: 'index.html',
 			    dest: 'build/',
 			    flatten: false
 			}
@@ -47,14 +58,15 @@ module.exports = function(grunt) {
 		}
 	});
 
-	  // Load plugins
-	  grunt.loadNpmTasks('grunt-contrib-uglify');
-	  grunt.loadNpmTasks('grunt-contrib-watch');
-	  grunt.loadNpmTasks('grunt-contrib-copy');
-	  grunt.loadNpmTasks('grunt-contrib-compress');
+	// Load plugins
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-express');
+	grunt.loadNpmTasks('grunt-open');
 
-
-	  // Default task(s).
-	  grunt.registerTask('default', ['uglify', 'copy', 'watch']);
-	  grunt.registerTask('build', ['uglify', 'copy', 'compress']);
+	// Default task(s).
+	grunt.registerTask('default', ['uglify', 'copy', 'express','open', 'watch']);
+	grunt.registerTask('build', ['uglify', 'copy', 'compress']);
 };
